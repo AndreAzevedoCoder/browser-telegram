eb1c9b6fcecb01b570868b1f47bddc174c35d5d4
Andre Azevedo <user@192.168.15.13>
2025-11-27 21:31:38 -0300

    this is somehting

--- background.js
+++ background.js
@@ -1,12 +1,41 @@
+// Source - https://stackoverflow.com/a
+// Posted by Hashbrown, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-11-27, License - CC BY-SA 4.0
 
-// const worker = new Worker(chrome.runtime.getURL('my-worker.js'));
-// console.log(worker)
-//sendSpoilerFromUrl("https://upload.wikimedia.org/wikipedia/commons/9/99/Black_square.jpg", 5);
-// chrome.webRequest.onHeadersReceived.addListener(
-//   function (details) {
-//     const headers = details.responseHeaders.filter(h => !/content-security-policy/i.test(h.name));
-//     return { responseHeaders: headers };
-//   },
-//   { urls: ["<all_urls>"] },
-//   ["blocking", "responseHeaders"]
-// );
+caches.keys().then(function(names) {
+    for (let name of names)
+        caches.delete(name);
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeNetRequest.updateDynamicRules(
+    {
+      // Remove old rule with same id (if it exists)
+      removeRuleIds: [1],
+      addRules: [
+        {
+          id: 1,
+          priority: 1,
+          action: {
+            type: "redirect",
+            redirect: {
+              // Path inside your extension
+              extensionPath: "/message-worker.js"
+            }
+          },
+          condition: {
+            // Match the exact Telegram JS file URL
+            regexFilter: "^https://web\\.telegram\\.org/a/2026.*\\.js$"
+          }
+        }
+      ]
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.error("Error adding DNR rule:", chrome.runtime.lastError);
+      } else {
+        console.log("Telegram JS override rule added");
+      }
+    }
+  );
+});
--- content.js
+++ content.js
@@ -5,10 +5,3 @@ s.src = chrome.runtime.getURL('inject.js');
 (document.head || document.documentElement).appendChild(s);
 s.remove();
 
-// const worker = new Worker(chrome.runtime.getURL("message-worker.js"));
-// console.log(worker)
-
-// In your content script
-// const workerUrl = chrome.runtime.getURL('message-worker.js');
-// const worker = new Worker(workerUrl);
-
